f2ec44d51ca3dcc9df5e9c38f4d09e1f7e524636
Celia Castaños <celica02@ucm.es>
2022-04-03 14:56:27 +0200

    First room generated and a few romms connected to it.

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -15,14 +15,16 @@ public class Generator : MonoBehaviour
     [SerializeField] private Cell baseCell;
     [SerializeField] private Sprite room;
     [SerializeField] private Sprite corridor;
+    int dimension;
 
     private List<List<Cell>> cells;
     private List<Cell> roomsWithAvailableDirections = new List<Cell>();
+    private Queue<Cell> roomsQueue = new Queue<Cell>();
 
     void Start()
     {
         // dimension = numRooms / 2 + 1 (de momento)
-        int dimension = Mathf.RoundToInt(numRooms / 2) + 1;
+        dimension = Mathf.RoundToInt(numRooms / 2) + 1;
         cells = new List<List<Cell>>(dimension);
         var rectTransform = GetComponent<RectTransform>();
         // casillas de 100x100, por lo que ponemos el tamaño del panel a 100xdimension
@@ -30,7 +32,7 @@ public class Generator : MonoBehaviour
         for (int i = 0; i < dimension; i++)
         {
             cells.Add(new List<Cell>(dimension));
-            for(int j = 0; j < dimension; j++)
+            for (int j = 0; j < dimension; j++)
             {
                 cells[i].Add(Instantiate(baseCell, transform));
                 // metemos las direcciones disponibles de cada casilla
@@ -40,10 +42,92 @@ public class Generator : MonoBehaviour
                 if (j < dimension - 1) cells[i][j].availableDirections.Add(Directions.Right);
             }
         }
+        Vector2 ini = new Vector2(Random.Range(0, dimension - 1), Random.Range(0, dimension - 1));
+        cells[(int)ini.x][(int)ini.y].GetComponent<Image>().sprite = room;
+        cells[(int)ini.x][(int)ini.y].type = Cell.CellType.Room;
+        roomsQueue.Enqueue(cells[(int)ini.x][(int)ini.y]);
+        CreateRoom(cells[(int)ini.x][(int)ini.y], (int)ini.x, (int)ini.y);
     }
 
     void Update()
     {
-        
+
     }
+    //Crea habitaciones respecto a la primera en la cola.
+    void CreateRoom(Cell cell, int posX, int posY)
+    {
+        int dirAv = roomsQueue.Peek().availableDirections.Count;
+        int nSalasConectadas = Random.Range(1, dirAv - 1);
+        Debug.Log("Número salas conectadas: " + nSalasConectadas);
+        for (int i = 0; i < nSalasConectadas; ++i)
+        {
+            int dirConection = Random.Range(0, dirAv - 1);
+            Directions dir = cell.availableDirections[dirConection];
+            int dist = 0, maxDist = 0;
+            int newX = 0, newY = 0;
+            switch (dir)
+            {
+                case Directions.Up:
+                    maxDist = posY;
+                    dist = Random.Range(1, maxDist);
+                    newX = posX;
+                    newY = posY - dist;
+                    break;
+                case Directions.Right:
+                    maxDist = (dimension - 1) - posX;
+                    dist = Random.Range(1, maxDist);
+                    newX = posX + dist;
+                    newY = posY;
+                    break;
+                case Directions.Down:
+                    maxDist = (dimension - 1) - posY;
+                    dist = Random.Range(1, maxDist);
+                    newX = posX;
+                    newY = posY + dist;
+                    break;
+                case Directions.Left:
+                    maxDist = posX;
+                    dist = Random.Range(1, maxDist);
+                    newX = posX - dist;
+                    newY = posY;
+                    break;
+            }
+            //Pintado habitación
+            cells[newX][newY].GetComponent<Image>().sprite = room;
+            cells[newX][newY].type = Cell.CellType.Room;
+            roomsQueue.Enqueue(cells[newX][newY]);
+
+            //Pintado pasillos
+            if (posX == newX) //Los pasillos son en vertical
+            {
+                for (int j = 1; j < dist; ++j)
+                {
+                    int increase = j;
+                    if (dir == Directions.Up)
+                        increase *= -1;
+                    cells[posX][posY + increase].GetComponent<Image>().sprite = corridor;
+                    cells[posX][posY + increase].transform.Rotate(0, 0, 90);
+                    cells[posX][posY + increase].type = Cell.CellType.Corridor;
+                }
+            }
+            else if (posY == newY) //Pasillos en horizontal
+            {
+                for (int j = 1; j < dist; ++j)
+                {
+                    int increase = j;
+                    if (dir == Directions.Left)
+                        increase *= -1;
+                    cells[posX + increase][posY].GetComponent<Image>().sprite = corridor;
+                    cells[posX + increase][posY].type = Cell.CellType.Corridor;
+                }
+            }
+        }
+    }
+
+    //Habitación aleatoriamente asignada a otra
+    void CreateRandomRoom()
+    {
+
+    }
+
 }
